183bf7df993e2cfa35c8d26b339251565a417660
Anmol-Jha <73248316+Anmol-Jha@users.noreply.github.com>
2023-03-30 00:36:33 +0545

    Add files via upload
    Dashboard related to flight and looks cool so you can change it as required

---
+++ Flights-Dashboard/Database-External/global.R
@@ -0,0 +1,58 @@
+library(shiny)                            # this packages is use for Shiny App
+library(shinydashboard)                   # this packages is use for Shiny Dashboard
+
+library(dplyr)                            # this packages is use for data preparation (edit, remove, mutate, etc)
+library(stringr)                          # all functions deal with "NA"'s and zero length vectors
+library(purrr)                            # requirement packages for Functional Programming Tools
+library(rlang)                            # requirement packages for Rmarkdown
+
+library(DT)                               # interface to the JavaScript library DataTables (https://datatables.net/)
+library(r2d3)                             # D3 visualization
+
+library(DBI)                              # this packages is use for Database System (DBS)
+library(dbplyr)                           # this packages is use for Database System (DBS)
+library(RSQLite)                          # this packages is use for Database System (DBS)
+library(RMySQL)                           # this packages is use to connect MySQL() 
+
+con <- dbConnect(MySQL(),
+                 user='factory',                           # please input your user account
+                 password='ApaAjaBoleh',                   # please input your password
+                 dbname='factory',                         # please input your dbname
+                 host='www.db4free.net')                   # Conneting R to your hosting
+
+# Note: Don't forget to run the following code (just for the first time) 
+# On your server run " SET GLOBAL local_infile = true " and possibly restart server
+# ----------------------------------------------------------------------
+
+# airlines=read.csv('airlines.csv', header = T,sep = ",")  # read data from your PC
+# airports=read.csv('airports.csv', header = T,sep = ",")  # read data from your PC
+# flights=read.csv('flights.csv', header = T,sep = ",")    # read data from your PC
+# 
+# dbWriteTable(con, "airlines", airlines)                  # Write data to your database
+# dbWriteTable(con, "airports", airports)                  # Write data to your database
+# dbWriteTable(con, "flights", flights)                    # Write data to your database
+
+airlines <- tbl(con, "airlines")                           # read data from your database
+airports <- tbl(con, "airports")                           # read data from your database
+flights  <- tbl(con, "flights")                            # read data from your database
+
+dbDisconnect(con)                                          # Disconnect from your database
+
+# Use purrr's split() and map() function to create the list
+# needed to display the name of the airline but pass its
+# Carrier code as the value
+
+airline_list <- airlines %>%
+  collect() %>%
+  split(.$name) %>%
+  map(~ .$carrier)
+
+# Use rlang's set_names() to easily create a valide "choices"
+# argument of the dropdown where the displayed text has to be
+# different than the value passed as the input selection
+
+month_list <- as.list(1:12) %>%
+  set_names(month.name)
+
+month_list$`All Year` <- 99
+
---
+++ Flights-Dashboard/README.md
@@ -0,0 +1,33 @@
+# About this project 
+
+![](https://raw.githubusercontent.com/Bakti-Siregar/Flights-Dashboard/master/dashboard.svg)
+
+This project is aimed to all students at Matana University who has enter the database system course with [Mr. Bakti Siregar, S.Si, M.Sc.](https://www.linkedin.com/in/bakti-siregar-15955480/)
+The project is used as a prerequisite for passing by the course, therefore students are expected to be able to provide the best creations they have learned during this semester and also suggest them to get any idea from other references as well.
+
+[Review the Flights-Dashboard Here](https://master-data-scientist.shinyapps.io/Flight-Dashboard/)
+
+# The Contents of Dashboard
+
+* Provide the Number Flights, Average Flights per day, and percentage of the Flights delayed
+* Monthly total flights and possibility to explore the detail by clicking the bar-chart
+* Top 10 Destination Airports Bar-Chart and possibility to explore the detail
+* User controller: the Airlines selection input (Sidebar of the dasboard)
+* User controller: the month selection input (Sidebar of the dasboard)
+* Source Code, About Me, and Youtube (My Explanation)
+
+# Your Challenge
+
+* Create your own shiny dashboard (Similar to my Dashboard)
+* By adding matana logo in the right corner of your dashboard [**Your score is 10%**]
+* Change the color box of the Number Flights, Average Flights per day, <br> and percentage of the Flights delayed) [**Your score is 10%**]
+* Create Top 10 Airlines Bar-Chart and possibility to explore the detail [**Your score is 10%**]
+* Please Sorting the Top 10 Airlines Bar-Chart from largest to smallest [**Your score is 10%**]
+* User controller: the Destination Airports selection input (Sidebar of the dasboard) [**Your score is 10%**]
+* User controller: the month selection input (Sidebar of the dasboard) [No Score]
+* Source Code, About Me, and Youtube (Your Explanation) [**Your score is 30%**]
+* Create your own dashboard using the **Complete data** in the Facatory database [**Your score is 20%**]
+
+# Disclaimer
+
+This application project is only for educational purposes. So, every opinion in this application is maight be not the same to the real description of the actual data, this is only a part of the imagination, creations, and just to get some ideas how to build a dashboard.
