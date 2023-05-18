e30262c4e072be5ecc6732676b9780333abebb48
Aayush Shrestha <03aayush10@gmail.com>
2023-05-18 18:16:04 +0545

    week 10 v2

--- Dasboard/dash2.R
+++ Dasboard/dash2.R
@@ -11,18 +11,41 @@ ui <- fluidPage(
                   choices = names(data), selected = "Survived")
     ),
     mainPanel(
-      plotOutput("plot", height = "400px"),
+      conditionalPanel(
+        condition = "typeof input.variable !== 'undefined' && isNaN(input.variable)",
+        plotOutput("pie_chart", height = "400px")
+      ),
+      conditionalPanel(
+        condition = "typeof input.variable !== 'undefined' && !isNaN(input.variable)",
+        plotOutput("histogram_plot", height = "400px")
+      ),
       tableOutput("table")
     )
   )
 )
 
 server <- function(input, output) {
-  output$plot <- renderPlot({
+  output$pie_chart <- renderPlot({
+    req(input$variable)
+    
+    if (!is.character(data[[input$variable]])) {
+      plot(NULL, xlim = c(0, 1), ylim = c(0, 1),
+           main = "Invalid Variable", xlab = "", ylab = "")
+      text(0.5, 0.5, "Please select a non-numeric variable.", cex = 1.5, col = "red", font = 2)
+    } else {
+      table_data <- data %>%
+        count(input$variable)
+      
+      pie(table_data$n, labels = table_data[[input$variable]], main = input$variable,
+          col = rainbow(length(table_data$n)), border = "white")
+    }
+  })
+  
+  output$histogram_plot <- renderPlot({
     req(input$variable)
     
     if (!is.numeric(data[[input$variable]])) {
-      plot(NULL, xlim = c(0, 1), ylim = c(0, 1), 
+      plot(NULL, xlim = c(0, 1), ylim = c(0, 1),
            main = "Invalid Variable", xlab = "", ylab = "")
       text(0.5, 0.5, "Please select a numeric variable.", cex = 1.5, col = "red", font = 2)
     } else {
