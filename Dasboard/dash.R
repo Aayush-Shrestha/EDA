1044c8100ed16be80e75b7de74e98db2a955ff4a
Aayush Shrestha <03aayush10@gmail.com>
2023-06-01 11:31:09 +0545

    2nd last
    nearly complete

--- Dasboard/dash.R
+++ Dasboard/dash.R
@@ -22,12 +22,12 @@ server <- function(input, output) {
     req(input$variable)
     
     if (!is.numeric(data[[input$variable]])) {
-      plot(NULL, xlim = c(0, 1), ylim = c(0, 1), 
-           main = "Invalid Variable", xlab = "", ylab = "")
-      text(0.5, 0.5, "Please select a numeric variable.", cex = 1.5, col = "red", font = 2)
+      variable_counts <- table(data[[input$variable]])
+      colors <- c("skyblue", "lightgreen", "orange", "pink", "yellow", "purple", "red")  # Add more colors as needed
+      labels <- paste(names(variable_counts), "(", variable_counts, ")", sep = " ")
+      pie(variable_counts, main = input$variable, col = colors, labels = labels)
     } else {
-      hist(data[[input$variable]], main = input$variable,
-           xlab = input$variable, col = "skyblue", border = "white")
+      hist(data[[input$variable]], main = input$variable, xlab = input$variable, col = "skyblue", border = "white")
       box()
     }
   })
